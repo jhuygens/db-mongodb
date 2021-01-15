080058301d3733395ebd21e2d2278c5f6e4c1ac0
Josué Girón <benjamingiron1@gmail.com>
2021-01-15 01:12:41 -0600

    Actualizar prefix en propiedades de estructura

--- users/structs.go
+++ users/structs.go
@@ -4,22 +4,22 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // User db info
 type User struct {
-	ID               primitive.ObjectID `json:"_id,omitempty"`
-	Email            string             `json:"email,omitempty"`
-	Password         string             `json:"password,omitempty"`
-	AppName          string             `json:"app_name,omitempty"`
-	ClientID         string             `json:"client_id,omitempty"`
-	CurrentSecretKey string             `json:"current_secret_key,omitempty"`
-	Token            string             `json:"token,omitempty"`
-	RefreshToken     string             `json:"refresh_token,omitempty"`
-	RedirectURLs     []RedirectURL      `json:"redirect_urls,omitempty"`
-	CreatedAt        string             `json:"created_at,omitempty"`
+	ID               primitive.ObjectID `bson:"_id,omitempty"`
+	Email            string             `bson:"email,omitempty"`
+	Password         string             `bson:"password,omitempty"`
+	AppName          string             `bson:"app_name,omitempty"`
+	ClientID         string             `bson:"client_id,omitempty"`
+	CurrentSecretKey string             `bson:"current_secret_key,omitempty"`
+	Token            string             `bson:"token,omitempty"`
+	RefreshToken     string             `bson:"refresh_token,omitempty"`
+	RedirectURLs     []RedirectURL      `bson:"redirect_urls,omitempty"`
+	CreatedAt        string             `bson:"created_at,omitempty"`
 }
 
 // RedirectURL of the user app
 type RedirectURL struct {
-	ID          primitive.ObjectID `json:"_id,omitempty"`
-	URL         string             `json:"url,omitempty"`
-	Description string             `json:"description,omitempty"`
-	CreatedAt   string             `json:"created_at,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	URL         string             `bson:"url,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	CreatedAt   string             `bson:"created_at,omitempty"`
 }
